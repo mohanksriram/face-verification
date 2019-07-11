56b39fed2c1078f29ebdcd5f806eee076d6e125b
Ishank <ishank.mittal@scientist.org.in>
2019-07-12 01:40:03 +0530

    1. inference working

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # face-verification
 Facial Verfication software based on Facenet
+
+
+## Setup
+
+Install Conda
+https://www.anaconda.com/distribution/
+
+Create a new environment and activate
+```
+conda create -n tf-keras python=3.7
+conda activate tf-keras
+```
+Clone the repo
+```
+git clone https://github.com/mohankumarSriram/face-verification.git
+```
+
+Download facenet model [here]('https://drive.google.com/open?id=1pwQ3H4aJ8a6yyJHZkTwtjcL4wYWQb7bn') and copy to the models folder
+
+Download Indian Celebrity Dataset [here]('https://drive.google.com/open?id=1oOgC0FmTda1NCGRQ_JkjMywzulZ5beeP')
+```
+mkdir models
+cp ~/Downloads/facenet_keras.h5 models/
+
+mkdir data
+cp ~/Downloads/custom.zip data/
+unzip data/custom.zip -d data/
+```
+
+
+Install requiremnts
+```
+pip install -r requirements.txt
+```
+
+
+## Load Dataset
+python load_data.py
+
+## Generate Embeddings
+python generate_embeddings.py
+
+## Train Classifier
+python train_classifier.py
+
+## Perform Inference
+python infer.py
---
+++ load_data.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+from os import listdir
+from os.path import isdir
+from PIL import Image
+
+from numpy import asarray
+from numpy import savez_compressed
+from numpy import load
+from numpy import expand_dims
+from random import choice
+
+
+import mtcnn
+from mtcnn.mtcnn import MTCNN
+
+DATA_PATH = Path("./data/custom")
+
+# extract a single face from a given photograph
+def extract_face(filename, required_size=(160, 160)):
+    # load image from file
+    image = Image.open(filename)
+    # convert to RGB, if needed
+    image = image.convert('RGB')
+    # convert to array
+    pixels = asarray(image)
+    # create the detector, using default weights
+    detector = MTCNN()
+    # detect faces in the image
+    results = detector.detect_faces(pixels)
+    # extract the bounding box from the first face
+    x1, y1, width, height = results[0]['box']
+    # bug fix
+    x1, y1 = abs(x1), abs(y1)
+    x2, y2 = x1 + width, y1 + height
+    # extract the face
+    face = pixels[y1:y2, x1:x2]
+    # resize pixels to the model size
+    image = Image.fromarray(face)
+    image = image.resize(required_size)
+    face_array = asarray(image)
+    return face_array
+
+# load images and extract faces for all images in a directory
+def load_faces(directory):
+    faces = []
+    # enumerate files
+    for filename in listdir(directory):
+        # path
+        path = directory + filename
+        # get face
+        face = extract_face(path)
+        # store
+        faces.append(face)
+    return faces
+
+def load_dataset(directory):
+    X, y = [], []
+    # enumerate folders, on per class
+    for subdir in listdir(directory):
+        # path
+        #print(f'{subdir}')
+        path = directory + subdir + '/'
+        #print(f'path is: {path}')
+        # skip any files that might be in the dir
+        if not isdir(path):
+            continue
+        # load all faces in the subdirectory
+        faces = load_faces(path)
+        # create labels
+        labels = [subdir for _ in range(len(faces))]
+        # summarize progress
+        print('>loaded %d examples for class: %s' % (len(faces), subdir))
+        # store
+        X.extend(faces)
+        y.extend(labels)
+    return asarray(X), asarray(y)
+
+
+def main():
+    # Load the training dataset
+    x_train, y_train = load_dataset(str(DATA_PATH) + '/train/')
+    print(f'Loaded training data - x_shape: {x_train.shape}, y_shape: {y_train.shape}')
+    x_test, y_test = load_dataset(str(DATA_PATH) + '/val/')
+    print(f'Loaded test data - x_shape: {x_test.shape}, y_shape: {y_test.shape}')
+    savez_compressed('5-indian-celebrity-faces-dataset.npz', x_train, y_train, x_test, y_test)
+    print('Data saved.')
+
+if __name__ == '__main__':
+    main()
+
